116af444891ae4700206824a21de78f775eb6fff
Brett Bim <osiris43@gmail.com>
2020-07-21 07:44:28 -0500

    hunts parsed

--- lib/draw_hunts/hunt_scraper.ex
+++ lib/draw_hunts/hunt_scraper.ex
@@ -2,77 +2,93 @@ defmodule DrawHunts.HuntScraper do
   alias DrawHunts.Hunt
 
   def scrape() do
-    hunts = get_hunts("ADE")
-    current_hunts = Enum.map(hunts, &parse_hunt/1)
-    final = Enum.reduce(current_hunts, [], fn(x, acc) -> duplicate(x, acc) end)
-    IO.inspect(final)
+    hunt_page = get_hunts("ADE")
+    category = Floki.find(hunt_page, ".package-title") |> Floki.text |> String.trim
+    current_hunts = Enum.map(Floki.find(hunt_page, "section"), fn(x) -> parse_hunt(x, category) end )
+    final = Enum.reduce(current_hunts, [], fn(h, acc) -> acc ++ duplicate_hunt(h) end )
+    IO.puts(Enum.count(final))
   end
 
   def get_hunts(category) do
     html = HTTPoison.get!("https://tpwd.texas.gov/huntwild/hunt/public/public_hunt_drawing/hunt-category-details.phtml?OCat=#{category}").body
     huntdata = Floki.parse(html)
-    Floki.find(huntdata, "section")
   end
 
-  def duplicate(hunt, acc) do
-    # for each date in the hunt, return a Map with all other attributes
-    # in the hunt plus the date
-    acc ++ Enum.map(hunt[:dates], fn(x) -> Map.merge(hunt, %{dates: x}) end )
+  def duplicate_hunt(hunt) do
+    Enum.map(hunt.alldates, fn(date) -> %{hunt | start_date: Enum.at(date, 0), end_date: Enum.at(date, 1)} end )
   end
-  def parse_hunt(hunt) do
-    hunt_data = %{}
-    hunt_data = Map.put(hunt_data, :location, Floki.find(hunt, ".title") |> Floki.text)
+  def parse_hunt(hunt_section, category) do
+    hunt =%Hunt{}
+    hunt = %{hunt | location: Floki.find(hunt_section, ".title") |> Floki.text,
+      category: category, type: "Special Permit Hunt"}
 
-    hunt_data = Enum.reduce(Floki.find(hunt, ".data-wrapper"), hunt_data, fn (attr, acc) ->
+    hunt = Enum.reduce(Floki.find(hunt_section, ".data-wrapper"), hunt, fn(attr, acc) ->
       hunt_attr =  Floki.find(attr, ".data-header") |> Floki.text
-      parsed = parse(hunt_attr, attr)
-      acc = Map.put(acc, elem(parsed, 0), elem(parsed, 1))
+      cells = Floki.find(attr, ".data-cell")
+      acc = parse(hunt_attr, cells, acc)
     end)
-    #acc = Map.put(acc, :location, Floki.find(hunt, ".title") |> Floki.text)
-    #acc = Map.put(acc, :dates, parse_huntdates(hunt))
-    #acc = Map.put(acc, :bag_limits, parse_baglimit(hunt))
+
   end
 
-  def parse("Hunt Dates", data_wrapper) do
-    huntdates = Floki.find(data_wrapper, ".data-cell")
-    |> Enum.map(&parse_single_huntdate/1)
-
-    {:dates, huntdates}
+  def parse("Hunt Dates", cells, hunt) do
+    huntdates = Enum.map(cells, &parse_single_huntdate/1)
+    %{ hunt | alldates: huntdates}
   end
 
-  def parse("Bag Limit", data_wrapper) do
-    limit_data = Floki.find(data_wrapper, ".data-cell")
-    |> Enum.at(0) # The first line only ever finds 1 so ?
+  def parse("Bag Limit", cells, hunt) do
+    limit_data = Enum.at(cells, 0) # The first line only ever finds 1 so ?
     |> Floki.children
     |> Enum.take_every(2)
     |> parse_limits([]) # TODO This should probably be a map or a reduce
 
-    {:bag_limits, limit_data}
+    %{hunt | bag_limit: limit_data}
   end
 
-  def parse(_, data_wrapper) do
-    {:not_used, "apple"}
+  def parse("Baiting", cells, hunt) do
+    baiting = Enum.at(cells, 0)
+    |> Floki.children
+    |> Floki.text
+    |> String.trim
+
+    if baiting == "Allowed", do: %{ hunt | baiting: true}, else: %{hunt | baiting: false}
   end
 
-  def parse_huntdates(hunt) do
-    huntdates = Floki.find(hunt, ".data-wrapper")
-    |> Enum.at(0)
-    |> Floki.find(".data-cell")
-    |> Enum.map(&parse_single_huntdate/1)
+  def parse("Hunt Method", cells, hunt) do
+    method = Enum.at(cells, 0)
+    |> Floki.children
+    |> Floki.text
+    |> String.trim
 
+    %{hunt | method: method}
   end
+
+  def parse("Permits", cells, hunt) do
+    # Todo both of these work and both are awful. Regex maybe?
+    available = Enum.at(cells, 0) |> Floki.text |> String.split(":") |> Enum.at(1) |> String.trim |> String.to_integer()
+    fee = Enum.at(cells, 1) |> Floki.text |> String.trim |> String.split("$") |> Enum.at(1)
+    |> String.split(".") |> Enum.at(0) |> String.to_integer()
+
+    %{hunt | permits: available, fee: fee}
+  end
+
+  def parse("Last Year", cells, hunt) do
+    applicants = Enum.at(cells, 0) |> Floki.text |> String.split(":") |> Enum.at(1) |> String.trim |> String.to_integer()
+    permits = Enum.at(cells, 1) |> Floki.text |> String.split(":") |> Enum.at(1) |> String.trim |> String.to_integer()
+    parsed = Enum.at(cells, 2) |> Floki.text |> String.split(":") |> Enum.at(1) |> String.trim()
+    success = 0
+    if String.ends_with?(parsed, "%"), do: success = String.slice(parsed, 0..-2) |> String.to_integer()
+
+    %{hunt | previous_year_applicants: applicants,
+      previous_year_permits: permits, previous_year_success: success}
+  end
+
+  def parse(_, _, hunt), do: hunt
 
   def parse_single_huntdate(data) do
-    a = Floki.children(data)
+    Floki.children(data)
     |> Enum.at(0)
     |> String.split("\u2013")
     |> Enum.map(fn(x) -> String.trim(x) end)
-  end
-
-  def parse_baglimit(hunt) do
-    {:ok, baglimit} = Floki.find(hunt, ".data-wrapper") |> Enum.fetch(1)
-    limit_data = Floki.find(baglimit, ".data-cell") |> Enum.at(0) |> Floki.children |> Enum.take_every(2)
-    limits = parse_limits(limit_data, [])
   end
 
   def parse_limits([head | tail], limits) do
